27d3d36fb088ca50be9d78cf7346060951063003
Robin Messenger <robin@bluenose.cs.dal.ca>
2018-01-22 14:04:59 -0400

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # sitemonitor
-Monitor a website for changes, and email (or text) those changes.
+
+This is a simple script to monitor a website for specific changes and
+send an email to a list of addresses containing the specific changes
+specified by a regular expression.
+
+## Installing
+This script requires a linux server with a functioning and configured mail
+server, cron daemon, and python2 or python3. To install, first clone this 
+repository:
+
+```git clone https://github.com/rmessenger/sitemonitor.git```
+
+Next, open sitemonitor.py in a text editor, and enter the URL of the site
+you wish to monitor, the regular expression which selects the subset
+of the HTML of interest, enter a list of email addresses to which
+notifications and errors can be sent, and enter the absolute path to
+the data and log files. Then you must add a cron job by
+calling:
+
+```crontab -e```
+
+and add an entry to automatically run this script on a recurring basis.
+For example, to run every half hour, enter this line:
+
+```*/30 * * * * /path/to/python /path/to/sitemonitor.py```
+
+Save the file, and you're done!
---
+++ sitemonitor.py
@@ -0,0 +1,101 @@
+# -------------------------------------------------------------------
+# SIMPLE SITE MONITORING SCRIPT
+# Robin Messenger
+# 
+# This script checks a specified website for a key string
+# and uses the unix "mail" command to send the relevant string
+# to a list of email addresses.
+# As an example, this script is configured to scan the city of
+# Halifax, NS service update page for the status of the winter
+# parking ban.
+# -------------------------------------------------------------------
+
+
+import urllib2
+import ssl
+import subprocess
+import re
+import datetime
+
+
+#List of email addresses to notify or send errors to
+NotifyEmails=['email@somesite.com']
+ErrorEmails=['email2@somesite.com']
+
+#Regular expression to search for, where parenthesized () group is
+#sent to notify email addresses
+MatchRE='Status:\s*</td>\s*<td[^>]*>\s*([^<]+)\s*</td>\s*</tr>'
+
+#Website to search
+SiteURL='https://www.halifax.ca/transportation/winter-operations/service-updates'
+
+#File to store whether or not we've informed user of change
+DataFilename='sitemonitor.dat'
+
+#File to store log
+LogFilename='sitemonitor.log'
+
+
+#A function to log messages
+def myLog(msg):
+	try:
+		with open(LogFilename,'a') as f:
+			f.write("[%s] %s\n" % (datetime.datetime.now(),msg))
+	except:
+		pass
+
+
+#A function to use "mail" command to send emails
+def sendMail(emailList,msg):
+	myLog(msg)
+	try:
+		p=subprocess.Popen("mail "+" ".join(emailList), 
+				   shell=True, stdin=subprocess.PIPE)
+		p.communicate(msg)
+		return True
+	except:
+		return False
+
+
+#A funtion to handle errors
+def myError(msg):
+	print("Error: "+msg)
+	sendMail(ErrorEmails,"Error: "+msg)
+
+
+try:    #Load HTML from page
+	ctx=ssl.create_default_context()
+	ctx.check_hostname=False
+	ctx.verify_mode=ssl.CERT_NONE
+	response=urllib2.urlopen(SiteURL, context=ctx)
+	html=response.read()
+
+	try:    #Read data file
+		lastStatus=""
+		with open(DataFilename,'r') as f:
+			lastStatus=f.read()
+
+		#See if we can find the status string
+		m = re.search(MatchRE, html)
+
+		try:    #Extract status line from page
+			currentStatus=m.group(1)
+
+			#If last status does not match current, text user
+			if currentStatus != lastStatus:
+				#Text me the new status, and if nothing goes wrong...
+				if sendMail(NotifyEmails,currentStatus):
+					try:    #Write most recent update to data file
+						with open(DataFilename,'w') as f:
+							f.write(currentStatus)
+					except:
+						myError("Problem writing to data file!")
+				else:
+					myError("Problem sending text message!")
+		except:
+			myError("Cannot find status on page!")
+
+	except:
+		myError("Problem reading data file!")
+except:
+	myError("Problem loading service update page!")
